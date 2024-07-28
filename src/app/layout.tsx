415a1b3a247a52624661e841a3334ea424c05880
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-07-28 14:31:55 +0200

    docs: insert metadata for SewPortfolioPage to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "My sew portfolio page",
-  description: "Generated to practice Next.js and build a fullstack shop",
+  title: 'My new portfolio page',
+  description: 'Generated to practice Next.js and build a fullstack shop',
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title as string}</title>
+        <meta name="description" content={metadata.description as string} />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
